f55f169fb0de6a418b668ce156b9eead38468575
chalana kalpitha <chalanakalpitha72@gmail.com>
2023-05-23 21:52:05 +0530

    navigation 1st part done

--- share_prompts/components/Nav.jsx
+++ share_prompts/components/Nav.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const isUserLoggedIn = true;
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Prompt logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
+
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+          </div>
+        ) : (
+          <div> </div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
