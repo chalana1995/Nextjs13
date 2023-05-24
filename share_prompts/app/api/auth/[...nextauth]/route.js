3e52352c5fe1c456c2f4ed0c3e95ff78542da350
chalana kalpitha <chalanakalpitha72@gmail.com>
2023-05-24 23:06:06 +0530

    add databse credintials

--- share_prompts/app/api/auth/[...nextauth]/route.js
+++ share_prompts/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectToDB } from "@utils/database";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
